9ee3a8857080c5abc5cc72a9e1316f9e0a8fed36
test lol <testlol@gmail.com>
2024-02-16 16:05:33 +0100

    Update fonction M2U_NameCustomer

--- app/Http/Controllers/api/prod/ApiProdM2UController.php
+++ app/Http/Controllers/api/prod/ApiProdM2UController.php
@@ -68,9 +68,9 @@ class ApiProdM2UController extends Controller
             }
 
             //Je convertis en tableau associatif
-            $element = json_decode($response, associative: false);
+            $element = json_decode($response, associative: true);
 
-            dd(Arr::has($element, "Wallets"), $element);
+            dd(Arr::has($element[0], "Wallets"), $element);
 
             if(!Arr::has($element, "Wallets")){ //On teste si l'utilisateur a un wallet actif
                 return response()->json([
