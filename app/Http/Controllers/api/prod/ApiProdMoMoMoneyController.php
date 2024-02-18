75743bc8341abfb50e3432eaa0d9b26237c352de
test lol <testlol@gmail.com>
2024-02-18 10:48:37 +0100

    Update fonction M2U_NameCustomer

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -927,7 +927,7 @@ class ApiProdMoMoMoneyController extends Controller
             ])->Get($http);
 
         $data = json_decode($response->body());
-
+dd($data);
         if($response->status()==200){
 
             if($data->status=="PENDING"){
