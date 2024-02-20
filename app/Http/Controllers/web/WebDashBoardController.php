60541d4af859da6570f6a907c2a5f82d2511b851
test lol <testlol@gmail.com>
2024-02-20 21:59:35 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/web/WebDashBoardController.php
+++ app/Http/Controllers/web/WebDashBoardController.php
@@ -90,7 +90,7 @@ class WebDashBoardController extends Controller
                 ->orderBy('ca', 'desc')
                 ->limit(5);
 
-          dd($bestAgents);
+          dd($bestAgents->get());
 
 /*            $resultGraphe= $query->selectRaw('year(kb_transactions.created_at) year, month(kb_transactions.created_at) month, sum(kb_transactions.debit) debit, sum(kb_transactions.credit) credit')
                 ->whereYear('transactions.created_at','=',Carbon::now()->year)
