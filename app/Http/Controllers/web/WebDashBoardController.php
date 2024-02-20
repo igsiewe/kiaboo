fba175f8fde41e37735f46d65c64f37f0771584d
test lol <testlol@gmail.com>
2024-02-20 23:27:37 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/web/WebDashBoardController.php
+++ app/Http/Controllers/web/WebDashBoardController.php
@@ -101,7 +101,10 @@ class WebDashBoardController extends Controller
                 }
                 else
                 {
-                    dd($data->envoi,$i );
+                    $result = array_filter($data, function ($item) {
+                        return $item["mois"] === $i;
+                    });
+                    dd($result);
                     $envoi->add($data["envoi"][$i]);
 
                 }
