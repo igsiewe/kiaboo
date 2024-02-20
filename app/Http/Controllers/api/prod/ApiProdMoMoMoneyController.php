5b9c87a93fc27c674b1b67ba7d109ce7765493ec
test lol <testlol@gmail.com>
2024-02-20 16:06:13 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -979,6 +979,7 @@ class ApiProdMoMoMoneyController extends Controller
     public function MomoCallBack(Request $request){
 
         $momocallBackResponse = file_get_contents('php://input');
+        dd($momocallBackResponse);
         $data = json_decode($momocallBackResponse);
         $externalId = $data->externalId;
         $Transaction = Transaction::where("id",$externalId);
