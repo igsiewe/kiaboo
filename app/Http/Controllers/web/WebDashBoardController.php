e10bf7a73b5d62b92bdd845da26e4790f599aab6
test lol <testlol@gmail.com>
2024-02-21 06:52:34 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/web/WebDashBoardController.php
+++ app/Http/Controllers/web/WebDashBoardController.php
@@ -103,7 +103,7 @@ class WebDashBoardController extends Controller
                 {
                     $data = json_encode($data);
 
-                    dd($data->envoi);
+                    dd($data->first()["envoi"]);
                     $envoi->add($data["envoi"][$i]);
 
                 }
