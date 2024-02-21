29e95aa79cd4ea223977046d4f34e643dbc88b90
test lol <testlol@gmail.com>
2024-02-21 12:07:17 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\api\ApiCommissionController;
 use App\Http\Controllers\api\ApiNotification;
 use App\Http\Controllers\Controller;
 use App\Http\Enums\ServiceEnum;
-use App\Http\Enums\StatusTransEnum;
 use App\Http\Enums\TypeServiceEnum;
 use App\Models\Transaction;
 use App\Models\User;
@@ -243,15 +242,66 @@ class ApiProdMoMoMoneyController extends Controller
 
         if($response->status()==202){
 
-                $checkStatus = $this->MOMO_Depot_Status( $accessToken, $subcriptionKey, $referenceID);
-                $datacheckStatus = json_decode($checkStatus->getContent());
+            $checkStatus = $this->MOMO_Depot_Status( $accessToken, $subcriptionKey, $referenceID);
+            $datacheckStatus = json_decode($checkStatus->getContent());
 
-                if($checkStatus->getStatusCode() !=200){
-                    return response()->json([
-                        'status'=>'error',
-                        'message'=>$datacheckStatus->message,
-                    ],$checkStatus->getStatusCode());
-                }
+            if($checkStatus->getStatusCode() !=200){
+                return response()->json([
+                    'status'=>'error',
+                    'message'=>$datacheckStatus->message,
+                ],$checkStatus->getStatusCode());
+            }
+            $financialTransactionId = $datacheckStatus->financialTransactionId;
+            //$dataResponse = json_decode($response->body());
+            try {
+                DB::beginTransaction();
+                //On Calcule la commission
+                $commission=json_decode($lacommission->getContent());
+                $commissionFiliale = doubleval($commission->commission_kiaboo);
+                $commissionDistributeur=doubleval($commission->commission_distributeur);
+                $commissionAgent=doubleval($commission->commission_agent);
+
+                $user = User::where('id', Auth::user()->id);
+                $balanceBeforeAgent = $user->get()->first()->balance_after;
+                $balanceAfterAgent = floatval($balanceBeforeAgent) - floatval($montant);
+                //on met à jour la table transaction
+
+                $Transaction = Transaction::where('id',$idTransaction)->where('service_id',$service)->update([
+                    'reference_partenaire'=>$referenceID, //$financialTransactionId,
+                    'balance_before'=>$balanceBeforeAgent,
+                    'balance_after'=>$balanceAfterAgent,
+                    'debit'=>$montant,
+                    'credit'=>0,
+                    'status'=>1, //End successfully
+                    'paytoken'=>$referenceID,
+                    'date_end_trans'=>Carbon::now(),
+                    'description'=>$datacheckStatus->description,
+                    'message'=>'Le dépôt a été effectué avec succès',
+                    'commission'=>$commission->commission_globale,
+                    'commission_filiale'=>$commissionFiliale,
+                    'commission_agent'=>$commissionAgent,
+                    'commission_distributeur'=>$commissionDistributeur,
+                ]);
+
+                //on met à jour le solde de l'utilisateur
+
+                //La commmission de l'agent après chaque transaction
+
+                $commission_agent = Transaction::where("fichier","agent")->where("commission_agent_rembourse",0)->where("source",Auth::user()->id)->sum("commission_agent");
+
+                $debitAgent = DB::table("users")->where("id", Auth::user()->id)->update([
+                    'balance_after'=>$balanceAfterAgent,
+                    'balance_before'=>$balanceBeforeAgent,
+                    'last_amount'=>$montant,
+                    'date_last_transaction'=>Carbon::now(),
+                    'user_last_transaction_id'=>Auth::user()->id,
+                    'last_service_id'=>ServiceEnum::DEPOT_OM->value,
+                    'reference_last_transaction'=>$reference,
+                    'remember_token'=>$referenceID,
+                    'total_commission'=>$commission_agent,
+                ]);
+
+                DB::commit();
 
                 $userRefresh = User::where('id', Auth::user()->id)->select('id', 'name', 'surname', 'telephone', 'login', 'email','balance_before', 'balance_after','total_commission', 'last_amount','sous_distributeur_id','date_last_transaction')->first();
                 $transactionsRefresh = DB::table('transactions')
@@ -260,20 +310,64 @@ class ApiProdMoMoMoneyController extends Controller
                     ->select('transactions.id','transactions.reference as reference','transactions.reference_partenaire','transactions.date_transaction','transactions.debit','transactions.credit' ,'transactions.customer_phone','transactions.commission_agent as commission','transactions.balance_before','transactions.balance_after' ,'transactions.status','services.name_service','services.logo_service','type_services.name_type_service','type_services.id as type_service_id','transactions.date_operation', 'transactions.heure_operation','transactions.commission_agent_rembourse as commission_agent')
                     ->where("fichier","agent")
                     ->where("source",Auth::user()->id)
-                    ->where('transactions.status',StatusTransEnum::VALIDATED->value)
+                    ->where('transactions.status',1)
                     ->orderBy('transactions.date_transaction', 'desc')
                     ->limit(5)
                     ->get();
 
+                $idDevice = $device;
+                $title = "Kiaboo";
+                $message = "Le dépôt MOMO de " . $montant . " F CFA a été effectué avec succès au ".$customerNumber;
+                $subtitle ="Success";
+                $appNotification = new ApiNotification();
+
+                $envoiNotification = $appNotification->sendNotificationPushFireBase($idDevice, $title, $subtitle, $message);
+
+                if($envoiNotification->status()==200){
+                    $resultNotification=json_decode($envoiNotification->getContent());
+                    $responseNotification=$resultNotification->response ;
+
+                    if($responseNotification->success==true){
+                        Log::info([
+                            'code'=> 200,
+                            'function' => "MOMO_Depot",
+                            'response'=>"Notification envoyée avec succès",
+                            'user' => Auth::user()->id,
+                        ]);
+                    }else{
+                        Log::error([
+                            'code'=> 500,
+                            'function' => "MOMO_Depot",
+                            'response'=>$resultNotification,
+                            'user' => Auth::user()->id,
+                        ]);
+                    }
+                }
+
                 return response()->json([
                     'success' => true,
-                    'message' => "SUCCESSFULL",
+                    'message' => "SUCCESSFULL", // $resultat->message,
                     'textmessage' =>"Le dépôt a été effectué avec succès", // $resultat->message,
-                    'reference' => Transaction::where("id",$idTransaction)->get()->first()->reference_partenaire, // $reference,
+                    'reference' => $reference,// $resultat->data->data->txnid,
                     'data' => [],// $resultat,
                     'user'=>$userRefresh,
                     'transactions'=>$transactionsRefresh,
+                //    'transactionsEnAttente'=>$transactionEnAttente,
                 ], 200);
+
+            }catch (\Exception $e) {
+                DB::rollback();
+                Log::error([
+                    'code'=> $response->status(),
+                    'function' => "MOMO_Depot",
+                    'response'=>$e->getMessage(),
+                    'user' => Auth::user()->id,
+                ]);
+                return response()->json([
+                    'success' => false,
+                    'message' => "Exception : Une exception a été détectée, veuillez contacter votre superviseur si le problème persiste", //'Une erreur innatendue s\est produite. Si le problème persiste, veuillez contacter votre support.',
+                ], 400);
+            }
 
         }else{
             Log::error([
@@ -282,12 +376,11 @@ class ApiProdMoMoMoneyController extends Controller
                 'response'=>$response->body(),
                 'user' => Auth::user()->id,
             ]);
-
             return response()->json(
                 [
                     'status'=>$response->status(),
                     'error'=>$response->body(),
-                    'message' => "Exception : Une exception a été détectée, veuillez contacter votre superviseur si le problème persiste", //'Une erreur innatendue s\est produite. Si le problème persiste, veuillez contacter votre support.',
+                    'message'=>$response->body(),
                 ],$response->status()
             );
         }
@@ -895,11 +988,7 @@ class ApiProdMoMoMoneyController extends Controller
 
         if($Transaction->count()>0){
             if($Transaction->first()->service_id ==ServiceEnum::RETRAIT_MOMO->value){
-                Log::info([
-                    'function' => "MOMO_Retrait",
-                    'response'=>$data,
-                    'user' => $agent,
-                ]);
+
                 if($data->status=="FAILED"){
                     $updateTransaction=$Transaction->update([
                         'status'=>3, // Le client n'a pas validé dans les délais et l'opérateur l'a annule
@@ -976,7 +1065,7 @@ class ApiProdMoMoMoneyController extends Controller
                         DB::rollBack();
                         Log::error([
                             'code'=> $e->getCode(),
-                            'function' => "MOMO_Retrait",
+                            'function' => "MOMO_Retrait_CheckStatus",
                             'response'=>$e->getMessage(),
                             'user' => $agent,
                             'referenceID' => $reference,
@@ -989,117 +1078,8 @@ class ApiProdMoMoMoneyController extends Controller
             }
 
             if($Transaction->first()->service_id ==ServiceEnum::DEPOT_MOMO->value){
-                Log::info([
-                    'function' => "MOMO_Depot",
-                    'response'=>$data,
-                    'user' => $agent,
-                ]);
-                if($data->status=="FAILED"){
-                    $updateTransaction=$Transaction->update([
-                        'status'=>3, // Le dépôt n'a pas aboutit
-                        'reference_partenaire'=>$data->financialTransactionId,
-                        'date_end_trans'=>Carbon::now(),
-                        'description'=>$data->status,
-                    ]);
-                }
-
-                if($data->status=="SUCCESSFUL") {
-                        //On Calcule la commission
-                        $functionCommission = new ApiCommissionController();
-                        $lacommission = $functionCommission->getCommissionByService(ServiceEnum::DEPOT_MOMO->value, $montant);
-                        $commission = json_decode($lacommission->getContent());
-                        $commissionFiliale = doubleval($commission->commission_kiaboo);
-                        $commissionDistributeur = doubleval($commission->commission_distributeur);
-                        $commissionAgent = doubleval($commission->commission_agent);
-                        $user = User::where('id', $Transaction->first()->created_by);
-                        $balanceBeforeAgent = $user->get()->first()->balance_after;
-                        $balanceAfterAgent = floatval($balanceBeforeAgent) - floatval($montant);
-                        $reference_partenaire = $data->financialTransactionId;
-                        $agent = $user->first()->id;
-                        $reference = $Transaction->first()->reference;
-                        $phoneCustomer = $Transaction->first()->customer_phone;
-                        $device_notification = $Transaction->first()->device_notification;
-                        try {
-                            DB::beginTransaction();
-                            //on met à jour la table transaction
-
-                            $updateTransaction = $Transaction->update([
-                                'reference_partenaire' => $reference_partenaire, //$financialTransactionId,
-                                'balance_before' => $balanceBeforeAgent,
-                                'balance_after' => $balanceAfterAgent,
-                                'debit' => $montant,
-                                'credit' => 0,
-                                'status' => 1, //End successfully
-                                'date_end_trans' => Carbon::now(),
-                                'description' => $data->status,
-                                'message' => 'Le dépôt a été effectué avec succès',
-                                'commission' => $commission->commission_globale,
-                                'commission_filiale' => $commissionFiliale,
-                                'commission_agent' => $commissionAgent,
-                                'commission_distributeur' => $commissionDistributeur,
-                            ]);
-
-                            //on met à jour le solde de l'utilisateur
-
-                            //La commmission de l'agent après chaque transaction
-
-                            $commission_agent = Transaction::where("fichier", "agent")->where("commission_agent_rembourse", 0)->where("source", $agent)->sum("commission_agent");
-
-                            $debitAgent = DB::table("users")->where("id", $agent)->update([
-                                'balance_after' => $balanceAfterAgent,
-                                'balance_before' => $balanceBeforeAgent,
-                                'last_amount' => $montant,
-                                'date_last_transaction' => Carbon::now(),
-                                'user_last_transaction_id' => Auth::user()->id,
-                                'last_service_id' => ServiceEnum::DEPOT_OM->value,
-                                'reference_last_transaction' => $reference_partenaire,
-                                'remember_token' => $reference,
-                                'total_commission' => $commission_agent,
-                            ]);
-
-                            DB::commit();
-
-                            $idDevice = $device_notification;
-                            $title = "Kiaboo";
-                            $message = "Le dépôt MOMO de " . $montant . " F CFA a été effectué avec succès au " . $phoneCustomer;
-                            $subtitle = "Success";
-                            $appNotification = new ApiNotification();
-
-                            $envoiNotification = $appNotification->sendNotificationPushFireBase($idDevice, $title, $subtitle, $message);
-
-                            if ($envoiNotification->status() == 200) {
-                                $resultNotification = json_decode($envoiNotification->getContent());
-                                $responseNotification = $resultNotification->response;
-
-                                if ($responseNotification->success == true) {
-                                    Log::info([
-                                        'code' => 200,
-                                        'function' => "MOMO_Depot",
-                                        'response' => "Notification envoyée avec succès",
-                                        'user' => Auth::user()->id,
-                                    ]);
-                                } else {
-                                    Log::error([
-                                        'code' => 500,
-                                        'function' => "MOMO_Depot",
-                                        'response' => $resultNotification,
-                                        'user' => Auth::user()->id,
-                                    ]);
-                                }
-                            }
-
-                        } catch (\Exception $e) {
-                            DB::rollback();
-                            Log::error([
-                                'code'=> $e->getCode(),
-                                'function' => "MOMO_Depot",
-                                'response'=>$e->getMessage(),
-                                'user' => $agent,
-                                'referenceID' => $reference,
-                            ]);
-
-                        }
-
+                if($data->status=="SUCCESSFUL"){
+                    $updateTransaction = $Transaction->update();
                 }
             }
 
