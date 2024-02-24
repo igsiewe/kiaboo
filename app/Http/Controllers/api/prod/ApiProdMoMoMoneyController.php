c04655859fdf8b7159758225852cfa6880464899
test lol <testlol@gmail.com>
2024-02-24 08:22:01 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -443,7 +443,7 @@ class ApiProdMoMoMoneyController extends Controller
                     'status'=>404,
                     'amount'=>$data->amount,
                     'externalId'=>$data->externalId,
-                    'message'=>$data->reason,
+                    'message'=>"Rassurez vous que le client n'ait pas atteint son nombre de transactions hebdomadaire, sinpn consultez votre support technique.",//$data->reason,
                     'description'=>$data->status,
                 ],404
             );
