f0437770b8450300fb7f305d406313db137e2432
test lol <testlol@gmail.com>
2024-02-25 11:56:55 +0100

    Remplace requesttopay by requesttoredraw

--- app/Http/Controllers/api/prod/ApiProdM2UController.php
+++ app/Http/Controllers/api/prod/ApiProdM2UController.php
@@ -1149,7 +1149,7 @@ class ApiProdM2UController extends Controller
                     "LoginName" => "CM949513",
                     "APIKey" => "oh09DFok0T4ecUz1kzw2o9SoVslEwE3eMpvgtpzrhE4uv",
                     "AppID" => "8SZpExWP0fxu6rKQEDva03KVT",
-                    "PIN" => "765639", //  $request->PIN,
+                    "PIN" => "SibSnfeSdksSji2023_@", //  $request->PIN,
                     "TransactionNumber" => $request->TransactionNumber,
                 ]  );
 
