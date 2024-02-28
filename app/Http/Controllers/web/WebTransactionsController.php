abf2d3ca1684b284a27dc721873631194a082873
test lol <testlol@gmail.com>
2024-02-28 17:09:56 +0100

    update function exportTransaction

--- app/Http/Controllers/web/WebTransactionsController.php
+++ app/Http/Controllers/web/WebTransactionsController.php
@@ -26,20 +26,9 @@ class WebTransactionsController extends Controller
 
     protected mixed $dataExport;
 
-    public function getDataExport(): mixed
-    {
-        return $this->dataExport;
-    }
-
-    public function setDataExport(mixed $dataExport): WebTransactionsController
-    {
+     public function __construct($dataExport){
         $this->dataExport = $dataExport;
-        return $this;
     }
-
-//    public function __construct($dataExport){
-//        $this->dataExport = $dataExport;
-//    }
 
 
     public function listTransactions(){
@@ -67,9 +56,9 @@ class WebTransactionsController extends Controller
         }
 
         $transactions  =$query->orderByDesc('transactions.date_transaction')->limit(100)->get();
-        dd($transactions);
-        $this->dataExport = $this->setDataExport($transactions);
-        dd($this->getDataExport());
+
+        $this->dataExport = $transactions;
+        dd($this->dataExport );
         $listagents =    $listagents->orderBy("name")->orderBy("surname")->get();
         return view('pages.transactions.transactions', compact('transactions','money','listagents','listpartenaires','listservices'));
     }
@@ -126,7 +115,7 @@ class WebTransactionsController extends Controller
         }
 
         $transactions  = $query->orderByDesc('transactions.date_transaction')->get();
-        $this->dataExport = $this->setDataExport($transactions);
+        $this->dataExport = $transactions;
 
         $listagents =$listagents->orderBy("name")->orderBy("surname")->get();
 
