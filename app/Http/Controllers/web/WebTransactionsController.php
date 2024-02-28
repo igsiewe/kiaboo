0c0605e798fc6b4b868c14ca4ed880e8c15d49ab
test lol <testlol@gmail.com>
2024-02-28 12:57:42 +0100

    update function exportTransaction

--- app/Http/Controllers/web/WebTransactionsController.php
+++ app/Http/Controllers/web/WebTransactionsController.php
@@ -388,7 +388,6 @@ class WebTransactionsController extends Controller
 
     public function exportTransaction(){
         // Export consumes only a few MB, even with 10M+ rows.
-        dd($this->transactionsGenerator()->getReturn() );
        return (new FastExcel( $this->transactionsGenerator() ))->export('transactions.xlsx');
     }
 
