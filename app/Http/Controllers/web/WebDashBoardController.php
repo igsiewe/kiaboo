6bba5a87f9c10cd072ce1ec71e348cb56822937c
test lol <testlol@gmail.com>
2024-02-29 14:52:18 +0100

    update controller WebTransactionsController

--- app/Http/Controllers/web/WebDashBoardController.php
+++ app/Http/Controllers/web/WebDashBoardController.php
@@ -83,7 +83,7 @@ class WebDashBoardController extends Controller
                 ->selectRaw('month(kb_transactions.date_transaction) as mois, sum(kb_transactions.debit) as envoi, sum(kb_transactions.credit) as retrait')
                 ->groupBy('mois')
                 ->orderBy('mois', 'desc')->get()->toArray();
-
+            dd($resultGraphe);
             $envoi = collect();
             $retrait = collect();
 
