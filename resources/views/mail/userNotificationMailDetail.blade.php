d3982adcc8a1ca06b6a59969082f654e7d7b1bb0
test lol <testlol@gmail.com>
2024-03-01 19:54:49 +0100

    update function ApiProdMoMoMoneyController

--- resources/views/mail/userNotificationMailDetail.blade.php
+++ resources/views/mail/userNotificationMailDetail.blade.php
@@ -9,8 +9,8 @@
 <br>Bonjour M./Mme {{ $data['name'] }}, </br>
 <br>Veuillez trouver ci-dessous vos paramètres de connexion au back-office de Kiaboo. </br><p><p/>
 Nom d'utilisateur : {{ $data['login'] }} <br/>
-Mot de passe :  {{ $data['password'] }} <br/><p><p/>
-<br>Merci de le changer dès votre première connexion</br>
+Mot de passe :  {{ $data['password'] }} <br/>
+<br>Merci de le changer dès votre première connexion</br><p><p/>
 Cordialement<br/>
 PS : Ceci est un mail automatique, merci de ne pas y répondre<br/><br/>
 <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('/assets/images/logoMail.png'))) }}" class="logo" alt="kiaboo">
