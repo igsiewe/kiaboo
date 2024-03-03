386e2d5edd18e08b0ba9920ca3e2c82355b2d3d7
test lol <testlol@gmail.com>
2024-03-03 19:28:18 +0100

    update WebDashBoard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Enums\UserRolesEnum;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        LogViewer::auth(function ($request){
+            return $request->user() && in_array($request->user()->type_user_id, [
+                UserRolesEnum::SUPADMIN->value, UserRolesEnum::ADMIN->value
+                ]);
+        });
     }
 }
