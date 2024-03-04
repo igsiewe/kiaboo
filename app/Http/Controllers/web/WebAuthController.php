5daa813cf01e0321da13f848e1331544333400e0
test lol <testlol@gmail.com>
2024-03-04 16:57:31 +0100

    integration Google Authenticator

--- app/Http/Controllers/web/WebAuthController.php
+++ app/Http/Controllers/web/WebAuthController.php
@@ -36,7 +36,7 @@ class WebAuthController extends BaseController
             return redirect()->route('2fa')->with('one_time_password', $one_time_password);
         }
 
-        return redirect()->intended('registers');
+        return view("google2fa.registers");
        // return redirect()->intended($this->redirectPath());
     }
 
