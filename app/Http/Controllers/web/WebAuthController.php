0efef79a12c72fdf6a660b0b241a78ea0d8c4cf0
test lol <testlol@gmail.com>
2024-03-04 23:21:43 +0100

    integration Google Authenticator

--- app/Http/Controllers/web/WebAuthController.php
+++ app/Http/Controllers/web/WebAuthController.php
@@ -15,15 +15,7 @@ class WebAuthController extends BaseController
 
 
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+
 
 //    public function authenticated(Request $request, $user)
 //    {
