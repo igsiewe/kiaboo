764bd4671beb1a67df84a95f3c3b763851062311
test lol <testlol@gmail.com>
2024-03-04 18:57:06 +0100

    integration Google Authenticator

--- resources/views/auth/2fa.blade.php
+++ resources/views/auth/2fa.blade.php
@@ -80,6 +80,13 @@
 </div>
 
 
+<!-- Javascripts -->
+<script src="{{asset("assets/plugins/jquery/jquery-3.4.1.min.js")}}"></script>
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="{{asset("assets/plugins/bootstrap/js/bootstrap.min.js")}}"></script>
+<script src="https://unpkg.com/feather-icons"></script>
+<script src="{{asset("assets/plugins/perfectscroll/perfect-scrollbar.min.js")}}"></script>
+<script src="{{asset("assets/js/main.min.js")}}"></script>
 
 </body>
 
