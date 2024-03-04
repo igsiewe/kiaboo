26fc5209ecd64b7df6d1095244620d3377588478
test lol <testlol@gmail.com>
2024-03-04 17:06:34 +0100

    integration Google Authenticator

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -75,7 +75,7 @@ class RegisterController extends Controller
     }
 
     public function register(Request $request){
-
+dd("dddddddddd");
         $this->validator($request->all())->validate();
 
         $google2fa = app('pragmarx.google2fa');
