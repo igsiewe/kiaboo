907d967041dc26ebe38d0b80f87312425c617188
test lol <testlol@gmail.com>
2024-03-04 22:29:02 +0100

    integration Google Authenticator

--- app/Http/Controllers/web/WebAuthController.php
+++ app/Http/Controllers/web/WebAuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
-use PragmaRX\Google2FA\Google2FA;
+use PragmaRX\Google2FALaravel\Google2FA;
 
 
 class WebAuthController extends BaseController
