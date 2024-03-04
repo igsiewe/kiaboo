501e8baa274b720a36caeeb6395751aaf417e148
test lol <testlol@gmail.com>
2024-03-04 17:22:41 +0100

    integration Google Authenticator

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,11 +50,14 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        dd($request->all());
+       // dd($request->all());
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+          //  'name' => ['required', 'string', 'max:255'],
+          //  'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'login'=>['required','email','max:255'],
+            'password' => ['required', 'string', 'min:8'],
+          //  'captcha' => 'required|captcha'
+           // 'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
