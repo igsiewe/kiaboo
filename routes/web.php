26265e2254763bc3a6866d3be4b4bbbcde7637fe
test lol <testlol@gmail.com>
2024-03-04 10:18:17 +0100

    integration Google Authenticator

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::any('/', [WebAuthController::class, 'login'])->name('login');
+Route::any('/login', [WebAuthController::class, 'login'])->name('login');
 //Route::middleware(['2fa'])->group(function(){
 //    Route::any('/login', [WebAuthController::class, 'login'])->name('logins');
 //    Route::post('/2fa', function(){
