6632e989eb696bedc1111c646aac7ebc4c0ad88f
test lol <testlol@gmail.com>
2024-03-07 11:25:36 +0100

    integration Google Authenticator

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -58,7 +58,7 @@
                         <a class="nav-link" id="sidebar-toggle" href="#"><i data-feather="arrow-left"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="{{route("siteweb")}}">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Settings</a>
--- routes/web.php
+++ routes/web.php
@@ -42,12 +42,11 @@ Route::middleware(['auth','checkStatus'])->group(function (){
 
     Route::any('/dashboard', [WebDashBoardController::class,'dashboard'])->name("dashboard");
     Route::any('/logout', [WebAuthController::class, 'logout'])->name('fermer');
-//    Route::get('/', function () {
-//        Session::flush();
-//        Auth::logout();
-//        $url = "https://kiaboo.net";
-//        return Redirect::to($url);
-//    })->name("siteweb");
+    Route::get('/', function () {
+        Session::flush();
+        Auth::logout();
+        return Redirect::to("https://kiaboo.net");
+    })->name("siteweb");
 
     Route::group(['prefix' => 'approvisionnement'], function () {
         Route::controller(WebApproAgentController::class)->group(function () {
