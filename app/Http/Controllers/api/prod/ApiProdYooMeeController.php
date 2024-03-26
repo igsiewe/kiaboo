14ca4f5abe6339cfa69accfcf3c8b6b46b2c0ee6
test lol <testlol@gmail.com>
2024-03-26 18:32:49 +0100

    YooMee

--- app/Http/Controllers/api/prod/ApiProdYooMeeController.php
+++ app/Http/Controllers/api/prod/ApiProdYooMeeController.php
@@ -22,7 +22,7 @@ class ApiProdYooMeeController extends Controller
         }
 
         $url = "http://quality-env.yoomeemoney.cm:8080/api/users?keywords=$customerPhone&roles=member&statuses=active";
-        $response = Http::withOptions(['verify' => false,])->withBasicAuth('kiaboo2024', 'ki@boo2024')
+        $response = Http::withBasicAuth('kiaboo2024', 'ki@boo2024')
             ->Get($url);
         if($response->status()==200){
             $json = json_decode($response, false);
