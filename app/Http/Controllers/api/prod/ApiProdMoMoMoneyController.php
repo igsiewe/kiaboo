503d1190671009bfe7fc99a9dc8b61c5f22a1e14
test lol <testlol@gmail.com>
2024-04-09 16:19:53 +0100

    Mise à jour Fonction depot MOMO et callBack MOMO

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -277,11 +277,11 @@ class ApiProdMoMoMoneyController extends Controller
 
             if($checkStatus->getStatusCode() !=200){
                 $updateTransaction=Transaction::where("id",$idTransaction)->update([
-                    'status'=>3, // Le dépôt n'a pas abouti, on passe en statut pending
+                    'status'=>2, // Le dépôt n'a pas abouti, on passe en statut pending
                     //'reference_partenaire'=>$data->financialTransactionId,
                     'date_end_trans'=>Carbon::now(),
                     'description'=>$datacheckStatus->description,
-                    'message'=>$datacheckStatus->message,
+                    'message'=>$datacheckStatus->message." - Vérifier le status dans la liste des en cours",
                 ]);
 
                 return response()->json([
@@ -508,7 +508,7 @@ class ApiProdMoMoMoneyController extends Controller
 
         $subcriptionKey = '1466a4536a3c476ab18baf82ce82a1f3';
 
-        $http = "https://proxy.momoapi.mtn.com/disbursement/v1_0/deposit/".$referenceId;
+        $http = "https://proxy.momoapi.mtn.com/disbursement/v1_0/transfer/".$referenceId;
 
         $response = Http::withOptions(['verify' => false,])->withHeaders(
             [
@@ -1008,14 +1008,95 @@ class ApiProdMoMoMoneyController extends Controller
                 );
             }
             if($data->status=="SUCCESSFUL"){
-                        return response()->json(
-                            [
-                                'status'=>200,
-                                'message'=>$data->status." - Transaction en succès",
-                                'response'=>$data
-                            ],200
-                        );
+
+                    $Transaction = Transaction::where('paytoken',$referenceID)->where('service_id',ServiceEnum::DEPOT_MOMO->value)->where('status',2);
+                    $idTransaction = $Transaction->first()->id;
+                    $service = $Transaction->first()->service_id;
+                    $montant = $Transaction->first()->debit;
+                    $user = User::where('id', Auth::user()->id);
+                    $agent = $user->first()->id;
+                    $reference = $Transaction->first()->reference;
+
+                    // On vérifie si les commissions sont paramétrées
+                    $functionCommission = new ApiCommissionController();
+                    $lacommission =$functionCommission->getCommissionByService($service,$montant);
+                    if($lacommission->getStatusCode()!=200){
+                        Log::info([
+                            'success' => false,
+                            'message' => "Impossible de calculer la commission",
+                            'service'=>$service,
+                            'montant'=>$montant,
+                        ]  );
                     }
+                    //On Calcule la commission
+                    $commission=json_decode($lacommission->getContent());
+                    $commissionFiliale = doubleval($commission->commission_kiaboo);
+                    $commissionDistributeur=doubleval($commission->commission_distributeur);
+                    $commissionAgent=doubleval($commission->commission_agent);
+
+                    $user = User::where('id', $agent);
+                    $balanceBeforeAgent = $user->get()->first()->balance_after;
+                    $balanceAfterAgent = floatval($balanceBeforeAgent) - floatval($montant);
+                    //on met à jour la table transaction
+
+                    $Transaction = Transaction::where('id',$idTransaction)->where('service_id',$service)->update([
+                        // 'reference_partenaire'=>$referenceID, //$financialTransactionId,
+                        'balance_before'=>$balanceBeforeAgent,
+                        'balance_after'=>$balanceAfterAgent,
+                        'debit'=>$montant,
+                        'credit'=>0,
+                        'status'=>1, //End successfully
+                        'date_end_trans'=>Carbon::now(),
+                        'description'=>$data->status,
+                        'message'=>'Le dépôt a été effectué avec succès',
+                        'commission'=>$commission->commission_globale,
+                        'commission_filiale'=>$commissionFiliale,
+                        'commission_agent'=>$commissionAgent,
+                        'commission_distributeur'=>$commissionDistributeur,
+                        'reference_partenaire'=>$data->financialTransactionId,
+                        'terminaison'=>'MANUAL',
+                    ]);
+
+                    //on met à jour le solde de l'utilisateur
+                    //La commmission de l'agent après chaque transaction
+
+                    $commission_agent = Transaction::where("fichier","agent")->where("commission_agent_rembourse",0)->where("source",$agent)->sum("commission_agent");
+
+                    $debitAgent = DB::table("users")->where("id", $agent)->update([
+                        'balance_after'=>$balanceAfterAgent,
+                        'balance_before'=>$balanceBeforeAgent,
+                        'last_amount'=>$montant,
+                        'date_last_transaction'=>Carbon::now(),
+                        'user_last_transaction_id'=>$agent,
+                        'last_service_id'=>ServiceEnum::DEPOT_OM->value,
+                        'reference_last_transaction'=>$reference,
+                        'remember_token'=>$reference,
+                        'total_commission'=>$commission_agent,
+                    ]);
+
+                    $userRefresh = User::where('id', Auth::user()->id)->select('id', 'name', 'surname', 'telephone', 'login', 'email','balance_before', 'balance_after','total_commission', 'last_amount','sous_distributeur_id','date_last_transaction')->first();
+                    $transactionsRefresh = DB::table('transactions')
+                        ->join('services', 'transactions.service_id', '=', 'services.id')
+                        ->join('type_services', 'services.type_service_id', '=', 'type_services.id')
+                        ->select('transactions.id','transactions.reference as reference','transactions.reference_partenaire','transactions.date_transaction','transactions.debit','transactions.credit' ,'transactions.customer_phone','transactions.commission_agent as commission','transactions.balance_before','transactions.balance_after' ,'transactions.status','transactions.service_id','services.name_service','services.logo_service','type_services.name_type_service','type_services.id as type_service_id','transactions.date_operation', 'transactions.heure_operation','transactions.commission_agent_rembourse as commission_agent')
+                        ->where("fichier","agent")
+                        ->where("source",Auth::user()->id)
+                        ->where('transactions.status',1)
+                        ->orderBy('transactions.date_transaction', 'desc')
+                        ->limit(5)
+                        ->get();
+
+                    return response()->json(
+                        [
+                            'status'=>200,
+                            'message'=>$data->status." - Transaction en succès",
+                            'user'=>$userRefresh,
+                            'transactions'=>$transactionsRefresh,
+                            'response'=>$data
+                        ],200
+                    );
+             }
+
         }else{
             return response()->json(
                 [
@@ -1141,12 +1222,7 @@ class ApiProdMoMoMoneyController extends Controller
                     ]);
                 }
                 if($data->status=="SUCCESSFUL"){
-//                    $updateTransaction = $Transaction->update([
-//                        'reference_partenaire'=>$data->financialTransactionId,
-//                        'terminaison'=>'CALLBACK',
-//                    ]);
 
-                //    DB::beginTransaction();
                     $idTransaction = $Transaction->first()->id;
                     $service = $Transaction->first()->service_id;
                     $montant = $data->amount;
@@ -1160,10 +1236,12 @@ class ApiProdMoMoMoneyController extends Controller
                     $functionCommission = new ApiCommissionController();
                     $lacommission =$functionCommission->getCommissionByService($service,$montant);
                     if($lacommission->getStatusCode()!=200){
-                        return response()->json([
+                        Log::info([
                             'success' => false,
                             'message' => "Impossible de calculer la commission",
-                        ], 400);
+                            'service'=>$service,
+                            'montant'=>$montant,
+                        ]  );
                     }
                     //On Calcule la commission
                     $commission=json_decode($lacommission->getContent());
@@ -1212,6 +1290,7 @@ class ApiProdMoMoMoneyController extends Controller
                         'remember_token'=>$reference,
                         'total_commission'=>$commission_agent,
                     ]);
+
 
                 }
             }
