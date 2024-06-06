16d7a76f9a7bd8da94dc89c8d7f693a77468a712
test lol <testlol@gmail.com>
2024-06-06 17:29:00 +0100

    integration swagger

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,6 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
         //Swagger
         Route::controller(ApiProdAuthController::class)->group(function (){
             Route::post('authenticate/auth','loginSwagger')->name("loginSwagger");
-            Route::post('/authenticate/changepassword','changePasswordSwagger')->name("changePasswordSwagger");
         });
 
 
@@ -71,7 +70,7 @@ Route::middleware('auth:api')->group(function () {
 
         //Swagger
         Route::controller(ApiProdAuthController::class)->group(function (){
-          //  Route::post('/authenticate/changepassword','changePasswordSwagger')->name("changePasswordSwagger");
+            Route::post('/authenticate/changepassword','changePasswordSwagger')->name("changePasswordSwagger");
         });
 
         Route::get('/sms/' ,[ApiSmsController::class,'index']) ;
