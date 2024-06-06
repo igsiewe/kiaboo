a7ab255d4115add443ba6861ac2c02e349ac45ce
test lol <testlol@gmail.com>
2024-06-06 11:42:45 +0100

    integration swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API Documentation",
+ *      description="API Documentation",
+ *      @OA\Contact(
+ *          email="devops@kiaboo.net"
+ *     ),
+ *     @OA\License(
+ *     name="Apache 2.0",
+ *     url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *    )
+ * )
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
+ * )
+ * @OA\Tag(
+ *     name="Auth",
+ *     description="Everything about your Auth"
+ * )
+ * @OA\PathItem(
+ *     path="/api/v1/user/login",
+ *     )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
