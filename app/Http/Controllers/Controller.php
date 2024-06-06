c247ce8c76625780f06eb525e851cd6aff9b2bcb
test lol <testlol@gmail.com>
2024-06-06 21:43:58 +0100

    integration swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,34 +7,34 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use OpenApi\Annotations as OA;
 
-///**
-// * @OA\Info(
-// *      version="1.0.0",
-// *      title="API Documentation",
-// *      description="API Documentation",
-// *      @OA\Contact(
-// *          email="devops@kiaboo.net"
-// *     ),
-// *     @OA\License(
-// *     name="Apache 2.0",
-// *     url="http://www.apache.org/licenses/LICENSE-2.0.html"
-// *    )
-// * )
-// * @OA\Server(
-// *     url=L5_SWAGGER_CONST_HOST,
-// *     description="API Server"
-// * )
-// * @OA\SecurityScheme(
-// *     type="http",
-// *     scheme="bearer",
-// *     securityScheme="bearerAuth"
-// * )
-// *
-// * @OA\PathItem(
-// *     path="/api/v1/user/login",
-// *     )
-// *
-// */
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API Documentation",
+ *      description="API Documentation",
+ *      @OA\Contact(
+ *          email="devops@kiaboo.net"
+ *     ),
+ *     @OA\License(
+ *     name="Apache 2.0",
+ *     url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *    )
+ * )
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     securityScheme="bearerAuth"
+ * )
+ *
+ * @OA\PathItem(
+ *     path="/api/v1/user/login",
+ *     )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
