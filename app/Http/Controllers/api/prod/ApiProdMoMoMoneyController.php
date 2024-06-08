061957f7c65f152e4d0851d9f1db26d2a58965e9
test lol <testlol@gmail.com>
2024-06-08 21:49:45 +0100

    integration Swagger

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -1430,9 +1430,9 @@ class ApiProdMoMoMoneyController extends Controller
         }
 
         // On vérifie si les commissions sont paramétrées
-       // $functionCommission = new ApiCommissionController();
-       // $lacommission =$functionCommission->getCommissionByService($service,$amount);
-        $lacommission=0;
+        $functionCommission = new ApiCommissionController();
+        $lacommission =$functionCommission->getCommissionByService($service,$amount);
+
         if($lacommission->getStatusCode()!=200){
             return response()->json([
                 'success'=>false,
