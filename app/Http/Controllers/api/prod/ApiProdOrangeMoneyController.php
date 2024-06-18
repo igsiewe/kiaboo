7c943075e7a7c235f79aae450a2b2dcd02d3b95f
test lol <testlol@gmail.com>
2024-06-18 13:03:45 +0100

    integration Swagger

--- app/Http/Controllers/api/prod/ApiProdOrangeMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdOrangeMoneyController.php
@@ -31,9 +31,9 @@ class ApiProdOrangeMoneyController extends Controller
         $this->channel="691301143";
         $this->pin="2222";
         $getTokenResponse = $this->OM_GetTokenAccess();
+        Log::info($getTokenResponse);
         if($getTokenResponse->status()==200){
             $dataToken = json_decode($getTokenResponse);
-            Log::info($dataToken);
             $this->token = $dataToken->access_token;
         }
     }
