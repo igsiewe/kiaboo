6e21dd5cd980a60688213335ad5186f94a3beb52
test lol <testlol@gmail.com>
2024-06-18 17:28:19 +0100

    integration Swagger

--- app/Http/Controllers/api/prod/ApiProdOrangeMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdOrangeMoneyController.php
@@ -342,7 +342,7 @@ class ApiProdOrangeMoneyController extends Controller
             "Content-Type"=>"application/json",
             "WSO2-Authorization"=>"Bearer ".$this->token,
 
-        ])->withOptions(['verify' => false,])->Post($url);
+        ])->withBody($data)->Post($url);
 
         Log::info([
             "Service"=>ServiceEnum::PAYMENT_OM->name,
