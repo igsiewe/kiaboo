6363f841aba40af2aca592008c2cf878e693f6ab
test lol <testlol@gmail.com>
2024-06-18 14:37:25 +0100

    integration Swagger

--- app/Http/Controllers/api/prod/ApiProdOrangeMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdOrangeMoneyController.php
@@ -322,26 +322,22 @@ class ApiProdOrangeMoneyController extends Controller
         $partenaire = Distributeur::where("id",Auth::user()->distributeur_id)->get()->first()->name_distributeur;
         $url = "https://omdeveloper-gateway.orange.cm/omapi/1.0.2/mp/init";
 
-        $response = Http::withOptions(['verify' => false,])
-            ->withHeaders([
-                    "accept"=>"application/json",
-                    "X-AUTH-TOKEN"=>$this->auth_x_token,
-                  //  "Content-Type"=>"application/json",
-                    "WSO2-Authorization"=>"Bearer ".$this->token,
-                ]
-            )
-            ->withBody(
-                [
-                     "notifUrl"=> "https://kiaboogroup.com/api/om/pm",
-                      "channelUserMsisdn"=> $this->channel,
-                      "amount"=> $amount,
-                      "subscriberMsisdn"=> $customer,
-                      "pin"=> $this->pin,
-                      "orderId"=> $request->marchandTransactionId,
-                      "description"=> "Transaction initie by ".$user->first()->telephone. " de ".$partenaire,
-                      "payToken"=> $payToken
-                ], 'application/x-www-form-urlencoded'
-            )
+        $response = Http::withHeaders([
+                "accept"=>"application/json",
+                "X-AUTH-TOKEN"=>$this->auth_x_token,
+                "Content-Type"=>"application/json",
+                "WSO2-Authorization"=>"Bearer ".$this->token,
+            ]
+        )->withOptions([
+            'verify' => false,
+            "notifUrl"=> "https://kiaboogroup.com/api/om/pm",
+            "channelUserMsisdn"=> $this->channel,
+            "amount"=> $amount,
+            "subscriberMsisdn"=> $customer,
+            "pin"=> $this->pin,
+            "orderId"=> $request->marchandTransactionId,
+            "description"=> "Transaction initie by ".$user->first()->telephone. " de ".$partenaire,
+            "payToken"=> $payToken])
             ->Post($url);
 
         Log::info([
