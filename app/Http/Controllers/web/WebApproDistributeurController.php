e6d66e1a811d9d12991794ebea7cda74b0da50a8
test lol <testlol@gmail.com>
2024-06-19 16:40:37 +0100

    integration swagger

--- app/Http/Controllers/web/WebApproDistributeurController.php
+++ app/Http/Controllers/web/WebApproDistributeurController.php
@@ -52,7 +52,8 @@ class WebApproDistributeurController extends Controller
             ->select('users.login as auteur','transactions.id','transactions.reference','transactions.reference_partenaire','transactions.date_transaction','transactions.debit','transactions.credit' ,'transactions.customer_phone','transactions.commission','transactions.commission_agent','transactions.commission_distributeur','transactions.balance_before','transactions.balance_after' ,'transactions.status','transactions.service_id','services.name_service','services.logo_service','type_services.name_type_service','type_services.id as type_service_id','transactions.date_operation', 'transactions.heure_operation')
             ->where("transactions.fichier","distributeur")
             ->where('transactions.status',1);
-        $listdistributeurs = Distributeur::all()->sortBy("name_distributeur");
+      //  $listdistributeurs = Distributeur::all()->sortBy("name_distributeur");
+        $listdistributeurs = Distributeur::where('application',1)->orderBy('name_distributeur');
         if(Auth::user()->type_user_id==UserRolesEnum::DISTRIBUTEUR->value){
             $listApprovisionnement = $listApprovisionnement->where("distributeur_id",Auth::user()->distributeur_id);
             $listOperation = $listOperation->where("transactions.source",Auth::user()->distributeur_id);
