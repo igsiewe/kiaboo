0a984cbdb5f20980f8e05439775e4e7a8328fc9d
test lol <testlol@gmail.com>
2024-07-02 14:48:55 +0100

    ApiProdTransactionController

---
+++ app/Models/remboursementPayment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class remboursementPayment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'date_demande',
+        'amount',
+        'description',
+        'created_by',
+        'updated_by',
+        'date_validation',
+        'motif_validation',
+        'motif_rejet',
+        'date_rejet',
+        'status',
+    ];
+}
