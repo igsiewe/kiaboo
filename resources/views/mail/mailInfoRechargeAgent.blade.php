f0f8982910bfceef6b7e31db20c56b07b70999b4
test lol <testlol@gmail.com>
2024-07-17 07:14:31 +0100

    Config mail Recharge agent

--- resources/views/mail/mailInfoRechargeAgent.blade.php
+++ resources/views/mail/mailInfoRechargeAgent.blade.php
@@ -17,7 +17,7 @@ Cordialement<br/>
 
 PS : Ceci est un mail automatique, merci de ne pas y répondre<br/><br/>
 {{--<img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('/assets/images/logoMail.png'))) }}" class="logo" alt="kiaboo">--}}
-<img src="data:image/png;base64,{{ base64_encode(file_get_contents(asset('/assets/images/logoMail.png')) ) }}" class="logo" alt="kiaboo">
-{{$image = base64_encode( file_get_contents(asset('/assets/images/logoMail.png') ))}}
+<img src="data:image/png;base64, {{ base64_encode(file_get_contents(asset('/assets/images/logoMail.png')) ) }}" class="logo" alt="kiaboo">
+
 </body>
 </html>
