fa5d99fa89a5ed7c0b2dba71a6788ed2c9f55b94
test lol <testlol@gmail.com>
2024-07-17 06:44:10 +0100

    Config mail Recharge agent

--- resources/views/mail/mailInfoRechargeAgent.blade.php
+++ resources/views/mail/mailInfoRechargeAgent.blade.php
@@ -6,14 +6,14 @@
 <body>
 <h1>{{ config('app.name')  }}</h1>
 
-<br>Bonjour M./Mme, </br>
-<br>Votre compte {{ $data['nameAgent'] }} vient d'être approvisionné. </br><p><p/>
+<br>Bonjour M./Mme, </br><p/>
+<br>Votre compte {{ $data['nameAgent'] }} vient d'être approvisionné par {{ $data['nameDistributeur'] }}</br><p><p/>
 IdTransaction : {{ $data['idTransaction'] }} <br/>
-Montant rechargé :  {{ $data['amount'] }} <br/>
-Nouveau solde :  {{ $data['newBalance'] }} <br/>
+Montant rechargé :  {{ $data['amount'] }} F CFA<br/>
+Nouveau solde :  {{ $data['newBalance'] }} F CFA<br/>
 <br>Merci pour votre confiance</br><p><p/>
 Cordialement<br/>
-{{ $data['nameDistributeur'] }} <br/>
+<br/>
 
 PS : Ceci est un mail automatique, merci de ne pas y répondre<br/><br/>
 <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('/assets/images/logoMail.png'))) }}" class="logo" alt="kiaboo">
