3b3364202803fa7764f39f7c4730e4d7a2de8abe
igsiewe <igsiewe@gmail.com>
2025-03-21 19:57:29 +0100

    Vérification Modele Ville

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -540,6 +540,7 @@ class ApiProdMoMoMoneyController extends Controller
         Log::info([
             'responseMoMoDepotStatus'=>$data,
         ]);
+        dd($data);
         $element = json_decode($response, associative: true);
         $externalId = $data->externalId;
         //On se rassure que la transaction est bien en status en attente
