400b4391f0f0cff6857face7a8bde78b4f06f5c1
igsiewe <igsiewe@gmail.com>
2025-03-21 19:23:55 +0100

    Vérification Modele Ville

--- app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
+++ app/Http/Controllers/api/prod/ApiProdMoMoMoneyController.php
@@ -57,7 +57,7 @@ class ApiProdMoMoMoneyController extends Controller
 
     public function MOMO_Disbursement_GetTokenAccess(){
 
-        $response = Http::withOptions(['verify' => false,])->withHeaders(['Ocp-Apim-Subscription-Key'=> 'c22f08082732417ea3ee479820813317'])->withBasicAuth('d51773a3-837d-4dd7-9413-2f82bc3c2de2', '6d2377a32f8a42918ccb4e8db1a51c64')
+        $response = Http::withOptions(['verify' => false,])->withHeaders(['Ocp-Apim-Subscription-Key'=> '1466a4536a3c476ab18baf82ce82a1f3'])->withBasicAuth('d51773a3-837d-4dd7-9413-2f82bc3c2de2', 'c22f08082732417ea3ee479820813317')
             ->Post('https://proxy.momoapi.mtn.com/disbursement/token/');
         if($response->status()==200){
             return response()->json($response->json());
