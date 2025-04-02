101bb78ac8a9b46d95f7644c30c4c5fcdc52e368
igsiewe <igsiewe@gmail.com>
2025-04-02 22:50:09 +0200

    Orange

--- app/Http/Controllers/api/prod/ApiOMController.php
+++ app/Http/Controllers/api/prod/ApiOMController.php
@@ -597,7 +597,7 @@ class ApiOMController extends Controller
         $responseTraiteRetraitOM = $this->OM_Retrait_execute($AccessToken, $payToken, $customerPhone, $request->amount, $idTransaction);
         if($responseTraiteRetraitOM->getStatusCode() !=200){
             $dataRetrait= json_decode($responseTraiteRetraitOM->getContent());
-            $texte = json_decode($dataRetrait->message);
+            $texte = $dataRetrait->message;
             return response()->json([
                 "result"=>false,
                 "message"=>"Exception ".$responseTraiteRetraitOM->getStatusCode()."\n".$texte
