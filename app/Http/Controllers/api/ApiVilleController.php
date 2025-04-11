1c81a9601cca802bf82dbd1dbce899d52808931b
igsiewe <igsiewe@gmail.com>
2025-04-11 21:52:01 +0200

    Quartier

--- app/Http/Controllers/api/ApiVilleController.php
+++ app/Http/Controllers/api/ApiVilleController.php
@@ -10,7 +10,7 @@ class ApiVilleController extends Controller
 {
   public function getListVille(){
       $villes = DB::table('villes')->where("status",1)->select("id","name_ville")->orderBy("name_ville")->get();
-      $quartiers = DB::table('quartiers')->where("status",1)->select("id","name_quartier")->orderBy("name_quartier")->get();
+      $quartiers = DB::table('quartiers')->where("status",1)->select("id","name_quartier","ville_id")->orderBy("name_quartier")->get();
         return response()->json([
             'status' => true,
             'message' => 'List of Villes',
