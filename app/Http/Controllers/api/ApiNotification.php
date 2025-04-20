55374e6e5781c3707b50c5d848920f9c2210ab49
igsiewe <igsiewe@gmail.com>
2025-04-20 16:33:57 +0200

    Updated

--- app/Http/Controllers/api/ApiNotification.php
+++ app/Http/Controllers/api/ApiNotification.php
@@ -32,6 +32,7 @@ class ApiNotification extends Controller
             ])->post($fcmurl, [
                 'message' => $notification,
             ]);
+            dd($response);
             return $response->json();
 
         }catch (\Exception $e) {
@@ -46,7 +47,6 @@ class ApiNotification extends Controller
         $scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
         $credentials = ApplicationDefaultCredentials::getCredentials($scopes);
         $token = $credentials->fetchAuthToken();
-        dd($token['access_token']);
         return $token['access_token'] ?? null;
     }
 
