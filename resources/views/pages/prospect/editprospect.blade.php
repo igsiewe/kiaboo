d726a810b2ef84fa94619fd104b3625fa7b29872
igsiewe <igsiewe@gmail.com>
2025-04-22 20:00:10 +0200

    Creation vue pour afficher les prospects

--- resources/views/pages/prospect/editprospect.blade.php
+++ resources/views/pages/prospect/editprospect.blade.php
@@ -14,14 +14,14 @@
                     <label class="form-label">Nom *</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-user"></i></span>
-                        <input type="text" id="name" name="name" class="form-control" placeholder="Nom *" aria-label="Name" value="{{$editProspect->name}}" required>
+                        <input type="text" id="name" name="name" disabled class="form-control" placeholder="Nom *" aria-label="Name" value="{{$editProspect->name}}" required>
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-6">
                     <label class="form-label">Prénom *</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-user"></i></span>
-                        <input type="text" id="surname" name="surname" class="form-control" value="{{$editProspect->surname}}" placeholder="Prénom *" aria-label="surname" required>
+                        <input type="text" id="surname" name="surname" disabled class="form-control" value="{{$editProspect->surname}}" placeholder="Prénom *" aria-label="surname" required>
                     </div>
                 </div>
             </div>
@@ -38,7 +38,7 @@
                     <label class="form-label">Email *</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-envelope"></i></span>
-                        <input type="text" id="email" name="email" class="form-control" placeholder="Email *" aria-label="email"  value="{{$editProspect->email}}"required>
+                        <input type="text" id="email" name="email" disabled  class="form-control" placeholder="Email *" aria-label="email"  value="{{$editProspect->email}}"required>
                     </div>
                 </div>
             </div>
@@ -48,14 +48,14 @@
                     <label class="form-label">Type pièce</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-id-card"></i></span>
-                        <input type="texte" id="typepiece"  name="typepiece" class="form-control" placeholder="Type pièce value="{{$editProspect->type_piece}}" aria-label="Type pièce" required>
+                        <input type="texte" id="typepiece"  name="typepiece" disabled class="form-control" placeholder="Type pièce value="{{$editProspect->type_piece}}" aria-label="Type pièce" required>
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-6">
                     <label class="form-label">Numéro pièce</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-id-card"></i></span>
-                        <input type="texte" id="numcni"  name="numcni" class="form-control" placeholder="Numéro CNI *" value="{{$editProspect->numero_piece}}" aria-label="Numéro pièce" required>
+                        <input type="texte" id="numcni"  name="numcni" disabled class="form-control" placeholder="Numéro CNI *" value="{{$editProspect->numero_piece}}" aria-label="Numéro pièce" required>
                     </div>
                 </div>
             </div>
@@ -65,14 +65,14 @@
                     <label class="form-label">Date validité</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-id-card"></i></span>
-                        <input type="date" id="dateValidite"  name="dateValidite" class="form-control" placeholder="Date validité" value="{{$editProspect->date_validite}}" aria-label="Date validité" required>
+                        <input type="date" id="dateValidite"  name="dateValidite" disabled class="form-control" placeholder="Date validité" value="{{$editProspect->date_validite}}" aria-label="Date validité" required>
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-6">
                     <label class="form-label">Ville édition</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-id-card"></i></span>
-                        <input type="text" id="villePiece"  name="villePiece" class="form-control" placeholder="Ville pièce" value="{{$editProspect->ville_piece->name_ville}}" aria-label="Ville édition" required>
+                        <input type="text" id="villePiece"  name="villePiece" disabled class="form-control" placeholder="Ville pièce" value="{{$editProspect->ville_piece->name_ville}}" aria-label="Ville édition" required>
                     </div>
                 </div>
             </div>
@@ -82,7 +82,7 @@
                     <label class="form-label">Ville</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-city"></i></span>
-                        <input type="text" id="quartier" name="quartier" class="form-control" value="{{$editProspect->quartier->ville->name_ville}}" placeholder="Ville" aria-label="Ville" required>
+                        <input type="text" id="quartier" name="quartier" disabled class="form-control" value="{{$editProspect->quartier->ville->name_ville}}" placeholder="Ville" aria-label="Ville" required>
                     </div>
                 </div>
 
@@ -90,7 +90,7 @@
                     <label class="form-label">Quartier</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-city"></i></span>
-                        <input type="text" id="quartier" name="quartier" class="form-control" value="{{$editProspect->quartier->name_quartier}}" placeholder="Quartier" aria-label="Quartier" required>
+                        <input type="text" id="quartier" name="quartier" disabled class="form-control" value="{{$editProspect->quartier->name_quartier}}" placeholder="Quartier" aria-label="Quartier" required>
                     </div>
                 </div>
             </div>
@@ -100,7 +100,7 @@
                     <label class="form-label">Adresse</label>
                     <div class="input-group mb-12">
                         <span class="input-group-text"><i class="fa fa-address-card"></i></span>
-                        <textarea type="text" id="adresse" name="adresse" maxlength="150" class="form-control" placeholder="Adresse" aria-label="Adresse" required>{{$editProspect->adresse}}</textarea>
+                        <textarea type="text" id="adresse" name="adresse" disabled maxlength="150" class="form-control" placeholder="Adresse" aria-label="Adresse" required>{{$editProspect->adresse}}</textarea>
                     </div>
                 </div>
             </div>
