cd8384816bf5b80e16ebfb3d4760d7c59715f6f5
igsiewe <igsiewe@gmail.com>
2025-04-22 22:09:22 +0200

    Creation vue pour afficher les prospects

--- app/Http/Controllers/web/WebProspectontroller.php
+++ app/Http/Controllers/web/WebProspectontroller.php
@@ -31,43 +31,43 @@ class WebProspectontroller extends Controller
        }
        try{
            DB::beginTransaction();
-           $user = user::find($id);
-           if($user){
-               if($user->status == 0){
+           $prospect = prospect::find($id);
+           if($prospect){
+               if($prospect->status == 0){
                    //desctive dans la table des prospects
                    $codeparrainage = new WebUtilisateurController();
-                   $user->status = 1;
-                   $user->validated_by = Auth::user()->id;
-                   $user->validated_at = Carbon::now();
-                   $user->save();
+                   $prospect->status = 1;
+                   $prospect->validated_by = Auth::user()->id;
+                   $prospect->validated_at = Carbon::now();
+                   $prospect->save();
 
                    //On ajoute le client dans la table des users
                    $newAgent = new User();
-                   $newAgent->name = $user->name;
-                   $newAgent->surname = $user->surname;
-                   $newAgent->password = $user->password;
-                   $newAgent->login =$user->phone;
+                   $newAgent->name = $prospect->name;
+                   $newAgent->surname = $prospect->surname;
+                   $newAgent->password = $prospect->password;
+                   $newAgent->login =$prospect->phone;
                    $newAgent->email_verified_at = Carbon::now();
                    $newAgent->created_by = Auth::user()->id;
-                   $newAgent->telephone = $user->phone_court;
-                   $newAgent->email = $user->email;
-                   $newAgent->quartier = quartier::where("id",$user->quartier_id)->first()->name_quarter;
-                   $newAgent->quartier_id = $user->quartier_id;
-                   $newAgent->adresse = $user->adresse;
+                   $newAgent->telephone = $prospect->phone_court;
+                   $newAgent->email = $prospect->email;
+                   $newAgent->quartier = quartier::where("id",$prospect->quartier_id)->first()->name_quarter;
+                   $newAgent->quartier_id = $prospect->quartier_id;
+                   $newAgent->adresse = $prospect->adresse;
                    $newAgent->type_user_id = UserRolesEnum::AGENT->value;
                    $newAgent->distributeur_id = 1;
                    $newAgent->countrie_id = 1;
                    $newAgent->status = 1;
-                   $newAgent->numcni = $user->numero_piece;
-                   $newAgent->datecni = $user->date_validite;
+                   $newAgent->numcni = $prospect->numero_piece;
+                   $newAgent->datecni = $prospect->date_validite;
                    $newAgent->seuilapprovisionnement="100000";
                    $newAgent->moncodeparrainage = "KI".$codeparrainage->genererChaineAleatoire(8);
                    $newAgent->save();
 
                    //On informe le client par SMS que son profil a été validé
                    $sms = new ApiSmsController();
-                   $tel =$user->phone;
-                   $msgBienvenue = $user->surname.", Votre compte KIABOO a été activé avec succès. Vous pouvez vous connecter dès à présent sur l'application KIABOO. Merci de changer votre mot de passe.";
+                   $tel =$prospect->phone;
+                   $msgBienvenue = $prospect->surname.", Votre compte KIABOO a été activé avec succès. Vous pouvez vous connecter dès à présent sur l'application KIABOO. Merci de changer votre mot de passe.";
                    $envoyerSMS = $sms->SendSMS($tel,utf8_decode($msgBienvenue));
                    $msgInfo = "Ne tardez plus, vous pouvez commencer à effectuer les transactions mobiles money et profiter de nombreux avantages.";
                    $envoyerSMS = $sms->SendSMS($tel,utf8_decode($msgInfo));
@@ -91,18 +91,18 @@ class WebProspectontroller extends Controller
        if(Auth::user()->type_user_id !=UserRolesEnum::SUPADMIN->value && Auth::user()->type_user_id !=UserRolesEnum::ADMIN->value && Auth::user()->type_user_id !=UserRolesEnum::BACKOFFICE->value){
            return redirect()->back()->withInput()->withErrors(['error' => "Vous n'êtes pas autorisés à acceder à cette page"]);
        }
-       $user = User::find($id);
-       if($user){
-           dd($user);
-           if($user->status == "0"){
-               $user->status = 2;//rejete
-               $user->validated_by = User::user()->id;
-               $user->validated_at = Carbon::now();
-               $user->save();
+       $prospect = prospect::find($id);
+       if($prospect){
+
+           if($prospect->status == "0"){
+               $prospect->status = 2;//rejete
+               $prospect->validated_by = User::user()->id;
+               $prospect->validated_at = Carbon::now();
+               $prospect->save();
 
                $sms = new ApiSmsController();
-               $tel =$user->phone;
-               $msgInfo = $user->surname.", Après analyse des éléments soumis via l'application KIABOO, nous n'avons pas été en mésure de valider votre compte KIABOO. Merci de vous rapprocher de l'agence KIABOO la plus proche";
+               $tel =$prospect->phone;
+               $msgInfo = $prospect->surname.", Après analyse des éléments soumis via l'application KIABOO, nous n'avons pas été en mésure de valider votre compte KIABOO. Merci de vous rapprocher de l'agence KIABOO la plus proche";
                $envoyerSMS = $sms->SendSMS($tel,utf8_decode($msgInfo));
 
                return redirect()->back()->with('success', 'Prospect rejeted successfully');
