bd7885d405622103b1054679ba35b3e73db430d4
igsiewe <igsiewe@gmail.com>
2025-05-10 17:35:37 +0200

    Gestion paiement marchand M2U

--- app/Http/Controllers/web/WebDashBoardController.php
+++ app/Http/Controllers/web/WebDashBoardController.php
@@ -97,9 +97,9 @@ class WebDashBoardController extends Controller
 
             $resultGraphe= $transAgent
                 ->whereYear('transactions.date_transaction', Carbon::now()->year)
-                ->selectRaw("month(kb_transactions.date_transaction) as mois, SUM(CASE WHEN kb_type_services.type_services = 'Payment' THEN credit ELSE 0 END) AS Paiement,
+                ->selectRaw("month(kb_transactions.date_transaction) as mois, SUM(CASE WHEN kb_type_services.name_type_service = 'Payment' THEN credit ELSE 0 END) AS Paiement,
                     SUM(CASE WHEN kb_type_services.name_type_service = 'Retait' THEN credit ELSE 0 END) AS Retait,
-                    SUM(CASE WHEN kb_type_services.type_services = 'Envoi' THEN debit ELSE 0 END) AS Envoi")
+                    SUM(CASE WHEN kb_type_services.name_type_service = 'Envoi' THEN debit ELSE 0 END) AS Envoi")
                 ->groupBy('mois')
                 ->orderBy('mois', 'desc')->get()->toArray();
 
