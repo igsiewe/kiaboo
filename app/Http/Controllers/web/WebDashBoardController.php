05fc0246e61e022ca62362f688842e1272442f0e
igsiewe <igsiewe@gmail.com>
2025-05-10 17:40:39 +0200

    Gestion paiement marchand M2U

--- app/Http/Controllers/web/WebDashBoardController.php
+++ app/Http/Controllers/web/WebDashBoardController.php
@@ -110,6 +110,7 @@ class WebDashBoardController extends Controller
             $paiement = collect();
 
             for ($i = 1; $i <= 12; $i++) {
+                dd($i);
                $data = collect($resultGraphe)->where("mois",$i);
                $envoi->add($data->sum(function ($op) use ($envoi){
                      return $op->envoi;
