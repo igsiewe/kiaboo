d7e0b9d07d6933c1e852d6dcf03f4ffedb14efd5
igsiewe <igsiewe@gmail.com>
2025-05-20 21:42:32 +0200

    Cron job

---
+++ app/Console/Commands/ArchiveLaravelLog.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+class ArchiveLaravelLog extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    //protected $signature = 'app:archive-laravel-log';
+    protected $signature = 'archive:log';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+   // protected $description = 'Command description';
+    protected $description = 'Archive laravel.log file daily and reset it';
+
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $logPath = storage_path('logs/laravel.log');
+
+        if (File::exists($logPath)) {
+            // Date de la veille
+            $yesterday = Carbon::yesterday()->format('Y-m-d');
+            $archivedPath = storage_path("logs/laravel-{$yesterday}.log");
+
+            // Renommer le fichier
+            File::move($logPath, $archivedPath);
+            $this->info("Fichier renommé en : laravel-{$yesterday}.log");
+
+            // Créer un nouveau fichier vide
+            File::put($logPath, '');
+
+            // Changer le propriétaire en "ubuntu"
+            // ATTENTION : nécessite que le script soit exécuté avec des droits sudo/crontab
+            exec("chown ubuntu:ubuntu {$logPath}");
+            exec("chmod 664 {$logPath}"); // lecture/écriture pour user et groupe
+
+            $this->info("Nouveau fichier laravel.log créé avec droits pour ubuntu");
+        } else {
+            $this->warn('Aucun fichier laravel.log trouvé à archiver.');
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Console;
 
+use App\Http\Controllers\api\ApiSmsController;
+use App\Http\Enums\UserRolesEnum;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,10 +19,34 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         //copier le fichier laravel.log tous les jours à minuit
-        $schedule->exec('sudo cp ./../../storage/logs/laravel.log ./../../storage/logs/laravel_$(date +\%Y\%m\%d\%H%M%S).log')->everyMinute();
-        $schedule->exec('sudo rm -rf ./../../storage/logs/laravel.log')->everyMinute();
-    //    $schedule->exec('touch ./../../storage/logs/laravel.log')->everyMinute();
+        //$schedule->exec('sudo cp ./../../storage/logs/laravel.log ./../../storage/logs/laravel_$(date +\%Y\%m\%d\%H%M%S).log')->everyMinute();
+        //$schedule->exec('sudo rm -rf ./../../storage/logs/laravel.log')->everyMinute();
+        //$schedule->exec('touch ./../../storage/logs/laravel.log')->everyMinute();
 
+        $schedule->command('archive:log')->dailyAt('00:00');
+
+        $schedule->call(function () {
+            $sms = new ApiSmsController();
+            //Parcourir la table des utilisateurs et recuperer le numéro de téléphone et le  solde (balanceAfter)
+            $users = User::where("type_user_id", UserRolesEnum::AGENT->value)->where("status",1)->where("id",36)->get();
+            foreach ($users as $user) {
+                $date_last_connexion = $user->last_connexion;
+                $now = Carbon::now();
+                if($date_last_connexion == null){
+                   $message = "M./Mme ".$user->lastname." ".$user->firstname."\nVotre compte KIABOO vous attend ! Connectez-vous dès maintenant et découvrez nos services.";
+                }else{
+                    $date_last_transaction = $user->date_last_transaction;
+                    $diffInDays = $date_last_transaction->diffInDays($now);
+                    if($diffInDays > 5){
+                        $message = "M./Mme ".$user->lastname." ".$user->firstname."\nVotre dernière transaction date du ".Carbon::parse($date_last_transaction)->format("dd/mm/YYYY H:i:s")." - Solde :".$user->balance_after." F CFA";
+                    }
+                }
+
+                $tel = ltrim($user->login,"+");
+                $msg = "M. ".$user->name." ".$user->surname."\nLe solde de votre compte Kiaboo est de ".$user->balance_after;
+                $envoyerSMS = $sms->SendSMS($tel,utf8_decode($msg));
+            }
+        })->everyMinute();
     }
 
     /**
