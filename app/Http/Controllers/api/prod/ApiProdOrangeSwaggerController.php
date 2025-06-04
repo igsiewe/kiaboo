d9243b9c99146f63d745e0c93bf2476f03c44522
igsiewe <igsiewe@gmail.com>
2025-06-04 18:01:22 +0200

    Momo functions

--- app/Http/Controllers/api/prod/ApiProdOrangeSwaggerController.php
+++ app/Http/Controllers/api/prod/ApiProdOrangeSwaggerController.php
@@ -29,32 +29,14 @@ class ApiProdOrangeSwaggerController extends Controller
      * description="This operation is used to request a payment from a consumer (Payer). The payer will be asked to authorize the payment. The transaction will be executed once the payer has authorized the payment. The requesttopay will be in status PENDING until the transaction is authorized or declined by the payer or it is timed out by the system. Status of the transaction can be validated by using the GET api/v1/prod/om/payment/<resourceId>",
      * security={{"bearerAuth":{}}},
      * tags={"OM - Payment"},
-     *     @OA\RequestBody(
-     *          required=true,
-     *        @OA\JsonContent(
-     *              required={"customerPhone","amount", "marchandTransactionId"},
-     *            @OA\Property(property="customerPhone", type="string", example="670000000"),
-     *             @OA\Property(property="amount", type="string", example="2500"),
-     *              @OA\Property(property="marchandTransactionId", type="string", example="TR-2025-0001"),
-     *          )
-     *      ),
-     * @OA\Response(
-     *    response=400,
-     *    description="Bad request",
+     * @OA\RequestBody(
+     *    required=true,
      *    @OA\JsonContent(
-     *       @OA\Property(property="success", type="boolean", example="false"),
-     *       @OA\Property(property="statusCode", type="string", example="ERR-INVALID-DATA-SEND"),
-     *       @OA\Property(property="message", type="string", example="Bad request, invalid data was sent in the request"),
-     *    )
-     * ),
-     * @OA\Response(
-     *      response=403,
-     *      description="you do not have the necessary permissions",
-     *      @OA\JsonContent(
-     *         @OA\Property(property="success", type="boolean", example="false"),
-     *         @OA\Property(property="statusCode", type="string", example="ERR-NOT-PERMISSION"),
-     *         @OA\Property(property="message", type="string", example="you do not have the necessary permissions"),
-     *      )
+     *        required={"customerPhone","amount", "marchandTransactionId"},
+     *        @OA\Property(property="customerPhone", type="string", example="670000000"),
+     *        @OA\Property(property="amount", type="string", example="2500"),
+     *        @OA\Property(property="marchandTransactionId", type="string", example="TR-2025-0001"),
+     *     )
      * ),
      * @OA\Response(
      *     response=422,
@@ -65,49 +47,6 @@ class ApiProdOrangeSwaggerController extends Controller
      *        @OA\Property(property="message", type="string", example="attribute not valid"),
      *     )
      *  ),
-     * @OA\Response(
-     *    response=202,
-     *    description="Payment initiated successfully",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="success", type="boolean", example="true"),
-     *       @OA\Property(property="statusCode", type="string", example="PAYMENT INITIATED"),
-     *       @OA\Property(property="message", type="string", example="payment initiate successfully"),
-     *       @OA\Property(property="paytoken", type="string", example="Payment token"),
-     *       @OA\Property(
-     *             type="object",
-     *             property="data",
-     *             @OA\Property(property="status", type="string", example="Transaction status"),
-     *             @OA\Property(property="transactionId", type="string", example="transacton id database"),
-     *             @OA\Property(property="dateTransaction", type="date", example="Date transaction"),
-     *             @OA\Property(property="amount", type="number", example="amount of transaction"),
-     *             @OA\Property(property="fees", type="number", example="transaction fees"),
-     *             @OA\Property(property="agent", type="string", example="agent who initiate transaction"),
-     *             @OA\Property(property="customer", type="number", example="customer phone number"),
-     *             @OA\Property(property="marchandTransactionID", type="number", example="id transaction of partner"),
-     *      )
-     *    ),
-     * ),
-     * @OA\Response(
-     *        response=208,
-     *        description="you do not have the necessary permissions",
-     *        @OA\JsonContent(
-     *           @OA\Property(property="success", type="boolean", example="false"),
-     *           @OA\Property(property="statusCode", type="string", example="ERR-MERCHAND-TRANSACTION-ID-DUPLICATE"),
-     *           @OA\Property(property="message", type="string", example="The transaction ID used by the merchant already exists"),
-     *            @OA\Property(
-     *            type="object",
-     *            property="data",
-     *            @OA\Property(property="status", type="string", example="Transaction status"),
-     *            @OA\Property(property="transactionId", type="string", example="transacton id database"),
-     *            @OA\Property(property="dateTransaction", type="date", example="Date transaction"),
-     *            @OA\Property(property="amount", type="number", example="amount of transaction"),
-     *            @OA\Property(property="fees", type="number", example="transaction fees"),
-     *            @OA\Property(property="agent", type="string", example="agent who initiate transaction"),
-     *            @OA\Property(property="customer", type="number", example="customer phone number"),
-     *            @OA\Property(property="marchandTransactionID", type="number", example="id transaction of partner"),
-     *            )
-     *        )
-     *   ),
      * @OA\Response(
      *    response=500,
      *    description="an error occurred",
