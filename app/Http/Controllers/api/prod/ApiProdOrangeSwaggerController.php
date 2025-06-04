85e8fb4842b51f4e470d06f50fa2175e15672775
igsiewe <igsiewe@gmail.com>
2025-06-04 17:40:20 +0200

    Momo functions

--- app/Http/Controllers/api/prod/ApiProdOrangeSwaggerController.php
+++ app/Http/Controllers/api/prod/ApiProdOrangeSwaggerController.php
@@ -28,7 +28,7 @@ class ApiProdOrangeSwaggerController extends Controller
      * summary="Request to make a OM payment",
      * description="This operation is used to request a payment from a consumer (Payer). The payer will be asked to authorize the payment. The transaction will be executed once the payer has authorized the payment. The requesttopay will be in status PENDING until the transaction is authorized or declined by the payer or it is timed out by the system. Status of the transaction can be validated by using the GET api/v1/prod/om/payment/<resourceId>",
      * security={{"bearerAuth":{}}},
-     * tags={"Merchant payment"},
+     * tags={"OM - Payment"},
      * @OA\RequestBody(
      *    required=true,
      *    description="Request to make a OM payment",
