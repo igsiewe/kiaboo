106ca4926a2f0b4fb9c905beb15e88a9f5bcef2f
igsiewe <igsiewe@gmail.com>
2025-06-04 15:34:28 +0200

    Momo functions

--- app/Http/Controllers/api/v1/fonctions/Orange_Controller.php
+++ app/Http/Controllers/api/v1/fonctions/Orange_Controller.php
@@ -2,10 +2,144 @@
 
 namespace App\Http\Controllers\api\v1\fonctions;
 
+use App\Http\Controllers\ApiLog;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class Orange_Controller extends Controller
 {
-    //
+    protected $token;
+    protected $auth;
+    protected $auth_x_token;
+    protected $channel;
+    protected $pin;
+    protected $url;
+    protected $endpoint;
+    protected $callbackUrl;
+
+    public function __construct()
+    {
+        $this->endpoint="https://api-s1.orange.cm/omcoreapis/1.0.2";
+        $this->callbackUrl="https://www.kiaboopay.com/api/callback/om/cico";
+        $this->auth="RllkYW1SbVAwWWNSUTlNbHRvdkd2NFBjTjlNYTpiTVdoeXFSNUtfZExOZ2ZRaHUzdmh0aV9ZZEFh"; //consumer_key et consumer_secret convertis en base64
+        $this->auth_x_token ="S0lBQk9PU0FSTEFQSU9NUFJPRDI1OktJQEJfT1NBUkxAUCFPbV9QUk9fZDIwMjU=";//api_username et api_password convertis en base64
+        $this->channel="691566672";
+        $this->pin="2025";
+    }
+
+    public function OM_GetTokenAccess()
+    {
+        $response = Http::withOptions(['verify' => false,])
+            ->withHeaders(
+                [
+                    "Authorization"=>"Basic ".$this->auth
+                ]
+            )
+            ->withBody('grant_type=client_credentials', 'application/x-www-form-urlencoded')
+            ->Post('https://api-s1.orange.cm/token');
+
+        if($response->status()==200){
+            return response()->json($response->json());
+        }
+        else{
+            return response()->json([
+                'status'=>'error',
+                'message'=>$response->body()
+            ],$response->status());
+
+        }
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/prod/om/customer/name/{customerPhone}",
+     *     summary="Get customer information",
+     *     tags={"OM - Customer information"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="customerPhone",
+     *         in="path",
+     *         required=true,
+     *         description="Customer phone number",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="Customer information",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example=true),
+     *              @OA\Property(property="firstname", type="string", example="DUPOND"),
+     *              @OA\Property(property="lastname", type="string", example="Hanry")
+     *          )
+     *      ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="An error occurred",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="boolean", example=false),
+     *             @OA\Property(property="message", type="string", example="Le numéro de téléphone n'est pas valide")
+     *         )
+     *     )
+     * )
+     */
+    public function OM_Customer($customerNumber)
+    {
+
+        $responseToken = $this->OM_GetTokenAccess();
+        if($responseToken->getStatusCode() !=200){
+            return $responseToken;
+        }
+        $dataAcessToken = json_decode($responseToken->getContent());
+        try{
+            $AccessToken = $dataAcessToken->access_token;
+            $token = $AccessToken;
+            $endpoint = $this->endpoint.'/infos/subscriber/customer/'.$customerNumber;
+
+            $response = Http::withOptions(['verify' => false,])
+                ->withHeaders(
+                    [
+                        'Content-Type'=> 'application/json',
+                        'X-AUTH-TOKEN'=>$this->auth_x_token,
+                        'Authorization'=>'Bearer '.$token
+                    ])
+                ->Post($endpoint, [
+                    "pin"=> $this->pin,
+                    "channelMsisdn"=> $this->channel
+                ]  );
+
+            if($response->status()==200){
+                $data = json_decode($response, false);
+                $firstName = $data->data->firstName;
+                $lastName = $data->data->lastName;
+                if($firstName==null || $firstName==""){
+                    return response()->json([
+                        'status' => false,
+                        'firstName' => $firstName,
+                        'lastName' => $lastName,
+                    ],404);
+                }
+                return response()->json([
+                    'status' => true,
+                    'firstName' => $firstName,
+                    'lastName' => $lastName,
+                ],200);
+            }else{
+                $body = json_decode($response->body());
+                return response()->json([
+                    'status' => false,
+                    'message'=> $body->message
+                ],$response->status());
+            }
+        }catch (\Exception $e){
+            return response()->json(
+                [
+                    'status'=> false,
+                    'messsage'=>  $e->getMessage(),
+
+                ],$e->getCode()
+            );
+        }
+
+    }
 }
