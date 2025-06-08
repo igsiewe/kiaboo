c4e6df18e06a2b0660e8d4c77201b63837b45d11
igsiewe <igsiewe@gmail.com>
2025-06-08 12:03:17 +0200

    Momo functions

--- app/Http/Controllers/web/WebServiceController.php
+++ app/Http/Controllers/web/WebServiceController.php
@@ -11,12 +11,12 @@ class WebServiceController extends Controller
 {
    public function getServicePartenaire($idPartenaire){
 
-       if($idPartenaire != null || $idPartenaire != "" || $idPartenaire != "0"  || $idPartenaire <> 0) {
-           $listservices = Service::where("partenaire_id", $idPartenaire)->orderBy("name_service")->get();
+       if($idPartenaire == null || $idPartenaire == "" || $idPartenaire == "0"  || $idPartenaire == 0) {
+           $listservices = Service::all()->sortBy("name_service");
            dd("1", $idPartenaire, $listservices);
        }
        else{
-           $listservices = Service::all()->sortBy("name_service");
+           $listservices = Service::where("partenaire_id", $idPartenaire)->orderBy("name_service")->get();
            dd("2", $idPartenaire, $listservices);
        }
 
