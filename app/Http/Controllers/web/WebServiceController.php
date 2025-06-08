ee5d39de53cd0919d580867983df0f01f2003c65
igsiewe <igsiewe@gmail.com>
2025-06-08 12:00:36 +0200

    Momo functions

--- app/Http/Controllers/web/WebServiceController.php
+++ app/Http/Controllers/web/WebServiceController.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\DB;
 class WebServiceController extends Controller
 {
    public function getServicePartenaire($idPartenaire){
-dd($idPartenaire);
-       if($idPartenaire != null || $idPartenaire != "" || $idPartenaire != "0") {
+
+       if($idPartenaire != null || $idPartenaire != "" || $idPartenaire != "0"  || $idPartenaire <> 0) {
 
            $listservices = Service::where("partenaire_id", $idPartenaire)->orderBy("name_service")->get();
        }
