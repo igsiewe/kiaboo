993c3472003129f418054bd4563c4429e1fb6e61
igsiewe <igsiewe@gmail.com>
2025-06-08 07:17:53 +0200

    Momo functions

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -122,22 +122,22 @@
 </div>
 
 {{--<script src="{{ asset('/sw.js') }}"></script>--}}
-<script>
-    if ("serviceWorker" in navigator) {
-        // Register a service worker hosted at the root of the
-        // site using the default scope.
-        navigator.serviceWorker.register("/sw.js").then(
-            (registration) => {
-                console.log("Service worker registration succeeded:", registration);
-            },
-            (error) => {
-                console.error(`Service worker registration failed: ${error}`);
-            },
-        );
-    } else {
-        console.error("Service workers are not supported.");
-    }
-</script>
+{{--<script>--}}
+{{--    if ("serviceWorker" in navigator) {--}}
+{{--        // Register a service worker hosted at the root of the--}}
+{{--        // site using the default scope.--}}
+{{--        navigator.serviceWorker.register("/sw.js").then(--}}
+{{--            (registration) => {--}}
+{{--                console.log("Service worker registration succeeded:", registration);--}}
+{{--            },--}}
+{{--            (error) => {--}}
+{{--                console.error(`Service worker registration failed: ${error}`);--}}
+{{--            },--}}
+{{--        );--}}
+{{--    } else {--}}
+{{--        console.error("Service workers are not supported.");--}}
+{{--    }--}}
+{{--</script>--}}
 
 <!-- Javascripts -->
 <script src="{{asset("assets/plugins/jquery/jquery-3.4.1.min.js")}}"></script>
