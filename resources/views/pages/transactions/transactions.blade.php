ced55bd719c0e2e150f72baaf86807405e11ea02
igsiewe <igsiewe@gmail.com>
2025-06-08 11:30:28 +0200

    Momo functions

--- resources/views/pages/transactions/transactions.blade.php
+++ resources/views/pages/transactions/transactions.blade.php
@@ -32,7 +32,7 @@
                                                 <label class="form-label" for="partenaire">Partenaires</label>
 
                                                     <select class="form-select"  id="partenaire" name="partenaire" onchange="javascript:getService(this.value)">
-                                                        <option>Tous les partenaires</option>
+                                                        <option value="">Tous les partenaires</option>
 
                                                         @if($listpartenaires)
                                                             @foreach($listpartenaires as $s)
