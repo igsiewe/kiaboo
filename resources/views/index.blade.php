24351f4ff0a19a9a9bf186fbff17a3575fc20d01
igsiewe <igsiewe@gmail.com>
2025-06-27 03:52:26 +0200

    PWA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
 
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
-    <link rel="apple-touch-icon" href="{{ asset('assets/images/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('/logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
 </head>
